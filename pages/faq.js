68ce557258ea22300b4e80dce4b66d8e1eb47eb9
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-18 14:26:56 -0300

    Creating component Link from nextJS's Link

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,10 @@
+import Link from "../src/components/Link";
+
 export default function FaqPage() {
     return (
         <div>
             <h1>Alura Cases - FAQ</h1>
-            <a href="/">Ir para a Home</a>
+            <Link href="/">Ir para a Home</Link>
         </div>
     )
 }
